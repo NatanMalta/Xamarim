872047121f0dfcd87a7ff245e0a662ebf59a0cb2
Natan Malta <natan.malta@hotmail.com>
2018-12-17 21:00:00 -0200

    .

---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsultarCEP.Serviço.Modelo;
+using App01_ConsultarCEP.Serviço;
+
+namespace App01_ConsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+
+            // TODO - Lógica d oprograma.
+
+
+
+            // TODO - validações.
+            string cep = CEP.Text.Trim();
+            Endereco end = ViaCepServiso.BuscarEnderecoViaCEP(cep);
+
+            RESULTADO.Text = string.Format("Lagradouro: {0}\nComplemento: {1}\nBairro: {2}\nCidade: {3}\nUF: {4}", end.logradouro, end.complemento, end.bairro, end.localidade, end.uf);
+        }
+
+    }
+
+
+
+}
